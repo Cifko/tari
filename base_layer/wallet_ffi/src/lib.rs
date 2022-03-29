becb042f614472b682d5f63719e133022094eecd
Martin Stefcek <gcifko@gmail.com>
2022-03-29 11:22:51 +0700

    remove unused allow(clippy)

--- base_layer/wallet_ffi/src/lib.rs
+++ base_layer/wallet_ffi/src/lib.rs
@@ -442,7 +442,6 @@ pub unsafe extern "C" fn byte_vector_get_at(ptr: *mut ByteVector, position: c_ui
         return 0u8;
     }
     let len = byte_vector_get_length(ptr, error_out) as c_int - 1; // clamp to length
-    #[allow(clippy::cast_sign_loss)]
     if len < 0 || position > len as c_uint {
         error = LibWalletError::from(InterfaceError::PositionInvalidError).code;
         ptr::swap(error_out, &mut error as *mut c_int);
@@ -1473,7 +1472,6 @@ pub unsafe extern "C" fn contacts_get_at(
         return ptr::null_mut();
     }
     let len = contacts_get_length(contacts, error_out) as c_int - 1;
-    #[allow(clippy::cast_sign_loss)]
     if len < 0 || position > len as c_uint {
         error = LibWalletError::from(InterfaceError::PositionInvalidError).code;
         ptr::swap(error_out, &mut error as *mut c_int);
@@ -1760,7 +1758,6 @@ pub unsafe extern "C" fn completed_transactions_get_at(
         return ptr::null_mut();
     }
     let len = completed_transactions_get_length(transactions, error_out) as c_int - 1;
-    #[allow(clippy::cast_sign_loss)]
     if len < 0 || position > len as c_uint {
         error = LibWalletError::from(InterfaceError::PositionInvalidError).code;
         ptr::swap(error_out, &mut error as *mut c_int);
@@ -1850,7 +1847,6 @@ pub unsafe extern "C" fn pending_outbound_transactions_get_at(
         return ptr::null_mut();
     }
     let len = pending_outbound_transactions_get_length(transactions, error_out) as c_int - 1;
-    #[allow(clippy::cast_sign_loss)]
     if len < 0 || position > len as c_uint {
         error = LibWalletError::from(InterfaceError::PositionInvalidError).code;
         ptr::swap(error_out, &mut error as *mut c_int);
@@ -1939,7 +1935,6 @@ pub unsafe extern "C" fn pending_inbound_transactions_get_at(
         return ptr::null_mut();
     }
     let len = pending_inbound_transactions_get_length(transactions, error_out) as c_int - 1;
-    #[allow(clippy::cast_sign_loss)]
     if len < 0 || position > len as c_uint {
         error = LibWalletError::from(InterfaceError::PositionInvalidError).code;
         ptr::swap(error_out, &mut error as *mut c_int);
